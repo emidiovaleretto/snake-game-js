8e5289add6b96010f1101c10c1d9d0b63f0a9d41
Emidio Valeretto <emidio.valereto@gmail.com>
2021-04-19 16:32:13 +0100

    defining reactions and preventing the snake from crossing the edge of the screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ snake[0] = {
     y: 8 * box
 };
 
-let direction = "";
+let direction = "right";
 
 function createBG() {
     context.fillStyle = "lightgreen";
@@ -21,25 +21,32 @@ function drawSnake() {
     }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event) {
+    if (event.KeyCode == 37 && direction != "right") direction = "left";
+    if (event.KeyCode == 38 && direction != "down") direction = "up";
+    if (event.KeyCode == 39 && direction != "left") direction = "right";
+    if (event.KeyCode == 40 & direction != "up") direction = "down";
+}
+
 function startGame() {
+
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+ 
     createBG();
     drawSnake();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if (direction == "right") {
-        snakeX += box;
-    }
-    if (direction == "left") {
-        snakeX -= box;
-    }
-    if (direction == "up") {
-        snakeY -= box;
-    }
-    if (direction == "down") {
-        snakeY += box;
-    }
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     snake.pop();
 
